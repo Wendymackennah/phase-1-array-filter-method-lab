6bb402e142c5dbf28783bce2a86bd0065fb46aca
wendymackennah <wendymackennah5@gmail.com>
2024-01-01 17:38:21 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution here
+//Function to find matching drivers
+    function findMatching(drivers, query) {
+  return drivers.filter(driver => driver.toLowerCase() === query.toLowerCase());
+}
+
+// Function to perform fuzzy matching
+function fuzzyMatch(drivers, letters) {
+  return drivers.filter(driver => driver.toLowerCase().startsWith(letters.toLowerCase()));
+}
+
+// Function to match driver names in an array of objects
+function matchName(driverObjects, name) {
+  return driverObjects.filter(driver => driver.name.toLowerCase() === name.toLowerCase());
+}
